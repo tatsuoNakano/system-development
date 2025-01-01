e2715bb3f821db6605289078f4d373118b4231c5
tatsuoNakano <aaaoauo1118@gmeil.com>
2025-01-02 01:10:26 +0900

    システム開発上流校庭WBSテンプレート完成　下流工程は上流工程が完成してから作成　これからガントチャート作成

--- 0.プロジェクト管理/スコープ管理/スコープ定義/WBSマインドマップ/6.保守.mm
+++ 0.プロジェクト管理/スコープ管理/スコープ定義/WBSマインドマップ/6.保守.mm
@@ -1,6 +1,7 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1735678648293" ID="ID_981201483" MODIFIED="1735683403513" TEXT="&#x4fdd;&#x5b88;">
-<node CREATED="1735683403971" ID="ID_1481927097" MODIFIED="1735683403971" POSITION="right" TEXT=""/>
+<node CREATED="1735683403971" ID="ID_1481927097" MODIFIED="1735708678793" POSITION="right" TEXT="&#x30d0;&#x30c3;&#x30af;&#x30a2;&#x30c3;&#x30d7;"/>
+<node CREATED="1735708679131" ID="ID_1758527008" MODIFIED="1735708686184" POSITION="right" TEXT="&#x30d0;&#x30fc;&#x30b8;&#x30e7;&#x30f3;&#x7ba1;&#x7406;"/>
 </node>
 </map>
--- 0.プロジェクト管理/スコープ管理/スコープ定義/WBSマインドマップ/7.運用.mm
+++ 0.プロジェクト管理/スコープ管理/スコープ定義/WBSマインドマップ/7.運用.mm
@@ -1,6 +1,8 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1735678648293" ID="ID_981201483" MODIFIED="1735683432082" TEXT="&#x904b;&#x7528;">
-<node CREATED="1735683432546" ID="ID_99526425" MODIFIED="1735683432546" POSITION="right" TEXT=""/>
+<node CREATED="1735683432546" ID="ID_99526425" MODIFIED="1735708591784" POSITION="right" TEXT="&#x53ce;&#x76ca;&#x5316;"/>
+<node CREATED="1735708592220" ID="ID_831709253" MODIFIED="1735708597823" POSITION="right" TEXT="&#x5e83;&#x544a;"/>
+<node CREATED="1735708598386" ID="ID_225578507" MODIFIED="1735708601968" POSITION="right" TEXT="&#x6700;&#x9069;&#x5316;"/>
 </node>
 </map>
